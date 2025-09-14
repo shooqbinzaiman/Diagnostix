885b5ca4ff2898f3cd992c026c366ce452ecef73
shooqbinzaiman <shooqshowqi@gmail.com>
2025-09-14 21:23:50 +0300

    Js2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const items = document.querySelectorAll(".draggable")
 const dropzones = document.querySelectorAll(".dropzone")
 const messageBox = document.getElementById("message-box")
+
+items.forEach((item) => {
+  item.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("text/plain", item.id)
+  })
+})
+
+dropzones.forEach((zone) => {
+  zone.addEventListener("dragover", (event) => {
+    event.preventDefault()
+  })
+})
