e2dedf286fa3e46e755aa41ca67768a697e74ecb
shooqbinzaiman <shooqshowqi@gmail.com>
2025-09-15 10:56:57 +0300

    html

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const items = document.querySelectorAll(".draggable")
-  const dropzones = document.querySelectorAll(".dropzone")
+  const dropZones = document.querySelectorAll(".dropzone")
   const messageBox = document.getElementById("message-box")
+  let correctItemsDropped = 0
 
   items.forEach((item) => {
     item.addEventListener("dragstart", (event) => {
@@ -9,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   })
 
-  dropzones.forEach((zone) => {
+  dropZones.forEach((zone) => {
     zone.addEventListener("dragover", (event) => {
       event.preventDefault()
     })
@@ -18,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault()
       const draggedId = event.dataTransfer.getData("text/plain")
       const draggedItem = document.getElementById(draggedId)
+      const totalCorrectItems = 3
 
       if (document.getElementById(draggedId)) {
         if (document.getElementById(draggedId).dataset.answer === "true") {
@@ -25,6 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
             event.target.appendChild(document.getElementById(draggedId))
             document.getElementById(draggedId).style.pointerEvents = "none"
             messageBox.textContent = "correct!"
+            correctItemsDropped++
+            console.log("correctItemsDropped", correctItemsDropped)
+            console.log("totalCorrectItems", totalCorrectItems)
+
+            if (correctItemsDropped == totalCorrectItems) {
+              console.log("here...")
+              window.location.replace("patient22.html")
+            }
           } else {
             messageBox.textContent = "This box already has an item!"
           }
