0e08e35e6ce5b0084be160b98cd27f60f6cfc063
shooqbinzaiman <shooqshowqi@gmail.com>
2025-09-16 15:18:29 +0300

    Adding audio and Start Game button in index.html

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const items = document.querySelectorAll(".draggable")
-  const dropZones = document.querySelectorAll(".dropzone")
-  const messageBox = document.getElementById("message-box")
-  let correctItemsDropped = 0
-
   items.forEach((item) => {
     item.addEventListener("dragstart", (event) => {
       event.dataTransfer.setData("text/plain", item.id)
     })
   })
 
-  dropZones.forEach((zone) => {
+  dropzones.forEach((zone) => {
     zone.addEventListener("dragover", (event) => {
       event.preventDefault()
     })
@@ -19,28 +14,21 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault()
       const draggedId = event.dataTransfer.getData("text/plain")
       const draggedItem = document.getElementById(draggedId)
-      const totalCorrectItems = 3
 
-      if (document.getElementById(draggedId)) {
-        if (document.getElementById(draggedId).dataset.answer === "true") {
-          if (zone.children.length === 0) {
-            event.target.appendChild(document.getElementById(draggedId))
-            document.getElementById(draggedId).style.pointerEvents = "none"
-            messageBox.textContent = "correct!"
-            correctItemsDropped++
-            console.log("correctItemsDropped", correctItemsDropped)
-            console.log("totalCorrectItems", totalCorrectItems)
+      if (draggedItem === null) {
+        return
+      }
 
-            if (correctItemsDropped == totalCorrectItems) {
-              console.log("here...")
-              window.location.replace("patient22.html")
-            }
-          } else {
-            messageBox.textContent = "This box already has an item!"
-          }
+      if (draggedItem.dataset.answer === "true") {
+        if (zone.children.length === 0) {
+          zone.appendChild(draggedItem)
+          draggedItem.style.pointerEvents = "none"
+          messageBox.textContent = "correct!"
         } else {
-          messageBox.textContent = "Wrong item! Try again"
+          messageBox.textContent = "This box already has an Item!"
         }
+      } else {
+        messageBox.textContent = "Wrong item! Try again"
       }
     })
   })
