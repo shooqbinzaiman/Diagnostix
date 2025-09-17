3175523dbd152be586214bb15c7f0bc28f91a37d
shooqbinzaiman <shooqshowqi@gmail.com>
2025-09-17 15:31:48 +0300

    Done with final touches

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let score = 0
+
 const goToCase = (caseNumber) => {
   if (caseNumber === 1) {
     window.location.href = "patient1.html"
@@ -9,10 +11,16 @@ const goToCase = (caseNumber) => {
     console.log("Case not found")
   }
 }
-const startGame = () => {
-  window.location.href = "cases.html"
+
+const addScore = () => {
+  score += 10
+  const scoreDisplay = document.querySelector("#score-display")
+  scoreDisplay.textContent = `Score: ${score}`
 }
 
+const completedCase = () => {
+  addScore(10)
+}
 const GoBack = () => {
   window.location.href = "cases.html"
 }
@@ -20,6 +28,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const items = document.querySelectorAll(".draggable")
   const dropZones = document.querySelectorAll(".dropzone")
   const messageBox = document.getElementById("message-box")
+  const scoreDisplay = document.getElementById("score-display")
+  const victoryMessage = document.getElementById("victory-message")
+
+  // if (scoreDisplay) {
+  //   scoreDisplay.textContent = "Score: " + getScore()
+  // }
+
+  // if (victoryMessage) {
+  // if (ansArr.length >= 3) {
+  //   console.log("win")
+
+  //   victoryMessage.textContent = "Well done! You healed all patients!"
+  // }
+  // }
 
   if (!items.length || !dropZones.length || !messageBox) {
     return
@@ -56,15 +78,16 @@ document.addEventListener("DOMContentLoaded", () => {
         if (zone.children.length === 0) {
           event.target.appendChild(document.getElementById(draggedId))
           document.getElementById(draggedId).style.pointerEvents = "none"
+          addScore()
           messageBox.textContent = "correct!"
           correctItemsDropped++
 
-          // console.log("correctItemsDropped", correctItemsDropped)
-          // console.log("totalCorrectItems", totalCorrectItems)
-
           if (correctItemsDropped == totalCorrectItems) {
-            messageBox.textContent =
-              "Well done! Click the Back button to heal the other cases DOC"
+            if (!localStorage.getItem("caseDone")) {
+              messageBox.textContent =
+                "Well done! You earned 10 points! Click the Back button to heal the other cases DOC!"
+              localStorage.setItem("caseDone", "true")
+            }
           }
         } else {
           messageBox.textContent = "This box already has an item!"
